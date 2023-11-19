4dadad98928d2b7f8cd03497aa5eafd76b5d08bd
bottlebear-github <pbw1283@gmail.com>
2023-11-19 22:25:35 +0900

    chore-paymentList 쿼리수정

--- src/main/java/com/hospital/erp/payment/PaymentDAO.java
+++ src/main/java/com/hospital/erp/payment/PaymentDAO.java
@@ -13,7 +13,7 @@ public interface PaymentDAO {
 	public int paymentInsert(PaymentVO paymentVO)throws Exception;
 	
 	//문서전체보기
-	public List<PaymentVO> paymentAllList1() throws Exception;
+	public List<PaymentVO> paymentAllList1(MemberVO memberVO) throws Exception;
 	
 	//로그인 한 사람의 문서 리스트
 	public List<PaymentVO> paymentList1(MemberVO memberVO) throws Exception;
